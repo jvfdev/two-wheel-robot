3f2b819b1a1869a35c364c1827388192a1d0463b
jvfdev <38409266+jvfdev@users.noreply.github.com>
2021-01-11 21:24:20 -0500

    Initial test code for MPU6050

---
+++ two-wheel-robot.ino
@@ -0,0 +1,75 @@
+//https://www.instructables.com/Arduino-Self-Balancing-Robot-1/
+
+
+ // MPU-6050 Short Example Sketch
+// By Arduino User JohnChi
+// August 17, 2014
+// Public Domain
+/* Wire Connections
+ * VCC - 5V
+ * GND - GND
+ * SCL - A5
+ * SDA - A4
+ * XDA - NC
+ * XCL - NC
+ * AD0 - NC
+ * INT - 2
+ */
+
+#include<Wire.h>
+#include<math.h>
+
+// MPU6050 Setup
+const int MPU_addr=0x68;  // I2C address of the MPU-6050
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+// calibration values
+float xCal = 16596.0;
+float yCal = 16845.0;
+float zCal = 17742.0;
+float AcXg;
+float AcYg;
+float AcZg;
+
+void setup(){
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+  Serial.begin(9600);
+}
+void loop(){
+  updateSensorValues();
+  Serial.print("X: ");
+  Serial.print(AcXg);
+  Serial.print(", Y: ");
+  Serial.println(AcYg);
+  delay(250);
+}
+
+void updateSensorValues(){
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+
+  AcXg = AcX/xCal;
+  AcYg = AcY/yCal;
+  AcZg = AcZ/zCal;
+  /*
+  Serial.print("AcX = "); Serial.print(AcX);
+  Serial.print(" | AcY = "); Serial.print(AcY);
+  Serial.print(" | AcZ = "); Serial.print(AcZ);
+  Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
+  Serial.print(" | GyX = "); Serial.print(GyX);
+  Serial.print(" | GyY = "); Serial.print(GyY);
+  Serial.print(" | GyZ = "); Serial.println(GyZ);
+  */
+  }
