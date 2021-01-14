875b263aefb8fd882a5bc0793466b1bc70d6fb11
jvfdev <38409266+jvfdev@users.noreply.github.com>
2021-01-13 20:30:55 -0500

    Added gyro angle measurement and loop time counter

--- two-wheel-robot.ino
+++ two-wheel-robot.ino
@@ -1,27 +1,28 @@
 //https://www.instructables.com/Arduino-Self-Balancing-Robot-1/
 
 
- // MPU-6050 Short Example Sketch
+// MPU-6050 Short Example Sketch
 // By Arduino User JohnChi
 // August 17, 2014
 // Public Domain
 /* Wire Connections
- * VCC - 5V
- * GND - GND
- * SCL - A5
- * SDA - A4
- * XDA - NC
- * XCL - NC
- * AD0 - NC
- * INT - 2
- */
+   VCC - 5V
+   GND - GND
+   SCL - A5
+   SDA - A4
+   XDA - NC
+   XCL - NC
+   AD0 - NC
+   INT - 2
+*/
 
 #include<Wire.h>
 #include<math.h>
 
 // MPU6050 Setup
-const int MPU_addr=0x68;  // I2C address of the MPU-6050
-int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+const int MPU_addr = 0x68; // I2C address of the MPU-6050
+int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ, gyroRate;
+float gyroAngle = 0;
 // calibration values
 float xCal = 16596.0;
 float yCal = 16845.0;
@@ -30,7 +31,9 @@ float AcXg;
 float AcYg;
 float AcZg;
 
-void setup(){
+unsigned long currTime, prevTime = 0, loopTime;
+
+void setup() {
   Wire.begin();
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x6B);  // PWR_MGMT_1 register
@@ -38,40 +41,48 @@ void setup(){
   Wire.endTransmission(true);
   Serial.begin(9600);
 }
-void loop(){
+void loop() {
   updateSensorValues();
+  currTime = millis();
+  loopTime = currTime - prevTime;
+  prevTime = currTime;
+  gyroRate = map(GyX, -32768, 32767, -250, 250);
+  gyroAngle = gyroAngle + (float)gyroRate*loopTime/1000.0;
   Serial.print("X: ");
   Serial.print(AcXg);
   Serial.print(", Y: ");
   Serial.println(AcYg);
-  float angle = atan2(AcYg, AcZg)*RAD_TO_DEG;
-  Serial.println(angle);
+  float angle = atan2(AcYg, AcZg) * RAD_TO_DEG;
+  Serial.print("acc ang: ");
+  Serial.print(angle);
+  Serial.print(", gyr0 ang: ");
+  Serial.print(gyroAngle);
   delay(250);
 }
 
-void updateSensorValues(){
+void updateSensorValues() {
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
-  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
-  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
-  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  Wire.requestFrom(MPU_addr, 14, true); // request a total of 14 registers
+  AcX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+  AcY = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp = Wire.read() << 8 | Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX = Wire.read() << 8 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY = Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
 
-  AcXg = AcX/xCal;
-  AcYg = AcY/yCal;
-  AcZg = AcZ/zCal;
+  AcXg = AcX / xCal;
+  AcYg = AcY / yCal;
+  AcZg = AcZ / zCal;
   /*
-  Serial.print("AcX = "); Serial.print(AcX);
-  Serial.print(" | AcY = "); Serial.print(AcY);
-  Serial.print(" | AcZ = "); Serial.print(AcZ);
-  Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
-  Serial.print(" | GyX = "); Serial.print(GyX);
-  Serial.print(" | GyY = "); Serial.print(GyY);
-  Serial.print(" | GyZ = "); Serial.println(GyZ);
+    Serial.print("AcX = "); Serial.print(AcX);
+    Serial.print(" | AcY = "); Serial.print(AcY);
+    Serial.print(" | AcZ = "); Serial.print(AcZ);
+    Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
+    Serial.print(" | GyX = "); Serial.print(GyX);
+    Serial.print(" | GyY = "); Serial.print(GyY);
+    Serial.print(" | GyZ = "); Serial.println(GyZ);
   */
-  }
+}
